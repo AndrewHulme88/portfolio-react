b44ddfac911a764472aecfe61443c990dc5bf9b5
andrew hulme <andrew_hulme04@hotmail.com>
2024-11-08 20:51:31 +1100

    Added links to github and linkedin

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
             <p>Software developer</p>
             <div className="social-icons">
               <a href="https://www.linkedin.com/in/andrew-hulme-b3a649291/"><i className="fab fa-linkedin"></i></a>
-              <a href="#"><i className="fab fa-github"></i></a>
+              <a href="https://github.com/AndrewHulme88"><i className="fab fa-github"></i></a>
               <a href="#"><i className="fab fa-slack"></i></a>
             </div>
           </div>
